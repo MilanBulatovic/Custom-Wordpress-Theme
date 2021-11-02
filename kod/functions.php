45e2911b5a19a74e142b12a9889d3c60804bbb48
Milan <milanbulatovic@outlook.com>
2021-11-02 15:12:15 +0100

    Update functions.php

--- kod/functions.php
+++ kod/functions.php
@@ -11,38 +11,15 @@ if ( ! defined( '_S_VERSION' ) ) {
 }
 
 if ( ! function_exists( 'kod_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
+	
 	function kod_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on KOD, use a find and replace
-		 * to change 'kod' to the name of your theme in all the template files.
-		 */
+		
 		load_theme_textdomain( 'kod', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
 		add_theme_support( 'title-tag' );
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
 		add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
@@ -78,7 +55,7 @@ if ( ! function_exists( 'kod_setup' ) ) :
 				'kod_custom_background_args',
 				array(
 					'default-color' => 'ffffff',
-					'default-image' => '',
+					'default-image' => ''
 				)
 			)
 		);
@@ -93,9 +70,7 @@ if ( ! function_exists( 'kod_setup' ) ) :
 		add_image_size( 'suggestion-thumb', 180, 125, true); // (cropped)
 
 		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
+		 * Add support for core custom logo
 		 */
 		add_theme_support(
 			'custom-logo',
